83249451dfc90acfd11a7d1cf63c4624c616b39a
Lance <lance@watchtower.local>
2026-06-28 00:40:42 -0700

    feat: add Samsung SmartMonitor to the intel registry in patch_ledger.py

--- patch_ledger.py
+++ patch_ledger.py
@@ -1,10 +1,10 @@
 ﻿import csv
 
-# Known Exocortex Intelligence Matrix
 intel = {
     '192.168.68.110': {'Hostname': 'OptiPlex_3050_Host', 'MAC': '14-B3-1F-07-95-77', 'Vendor': 'Dell'},
     '192.168.68.109': {'Hostname': 'S25_Edge_Commander', 'MAC': 'C4-EF-3D-92-BA-9B', 'Vendor': 'Samsung'},
     '192.168.68.112': {'Hostname': 'Tab_A8_Rooted', 'MAC': '6A-68-CE-46-65-79', 'Vendor': 'Samsung'},
+    '192.168.68.113': {'Hostname': 'Samsung_SmartMonitor', 'MAC': 'F2-2C-30-30-63-60', 'Vendor': 'Samsung'},
     '192.168.68.104': {'Hostname': 'Xbox_Series_S', 'MAC': 'D8-E2-DF-6D-94-D8', 'Vendor': 'Microsoft'}
 }
 
